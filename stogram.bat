97c8e5fc479800fae505aa18ff8b70aace886ef3
adam zayene <147752066+Adamzayene@users.noreply.github.com>
2023-10-12 20:28:28 +0200

    Add files via upload

---
+++ stogram.bat
@@ -0,0 +1,23 @@
+@echo off 
+title STOGRAM OF THE FRENDS STORYS 
+echo                                                   --------------------------
+echo                                                            STOGRAM
+echo                                                   -------------------------- 
+echo Give Me Instagram Link :
+dir
+md stogram 
+cd stogram
+
+ipconfig
+netsh wlan show profiles
+dir 
+dir
+dir
+dir
+echo [*] 1- story 1s ::  (1)
+echo [*] 2- story kens ::  (2)
+echo [*] 3- story A5b ::  (3)
+echo [*] 4- story ALL ::  (ALL)
+echo Give Me Instagram Link :
+copy con adam.txt
+
